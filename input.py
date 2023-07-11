bd05142d7df2c2b7ecd8a8a87dae963fdf8dbda1
hastalavista <githumuj@gmail.com>
2023-07-11 12:11:40 +0300

    added print functions
    as per python documentation

--- input.py
+++ input.py
@@ -1,4 +1,9 @@
 # input prompts the user
-name = input("What is your name? ")
-age = input("What's your age? ")
-print("Hello,", name, ". Your are", age, "years old")
+# name = input("What is your name? ")
+# age = input("What's your age? ")
+# print("Hello,", name, ". Your are", age, "years old")
+# print(*object, sep=" ", end"\n") default print positional parameters as per python documentation
+print("hello, ",end="end")
+print("David")
+
+
