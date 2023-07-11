f4c6b58465d1a2357c51e8d1250d9c9fab966472
hastalavista <githumuj@gmail.com>
2023-07-11 15:44:04 +0300

    added float, int and round methods

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 # this program contains int functions and methods
+# INT
 # python takes keyboard input as strings even if number values are keyed in
 # int()function converts the input from str ito int
 # a = int(input("Enter first number: "))
@@ -7,5 +8,16 @@
 
 # print(a + b)
 
-# the
-print(int(input("Enter first number: ")) + int(input("Enter second number: ")))
+# the lines above can be summarized as below
+# print(int(input("Enter first number: ")) + int(input("Enter second number: ")))
+
+# FLOAT
+# float function allows processing of decimal numbers
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# round(number[, ndigits]), this rounds up decimal to the nearest integer, ndigits specifies number of decimal places
+c = round(a + b)
+
+# print(f"{c:,}") this puts a comma after every third digit
+print(f"{c:.2f}") # this specifies that the output should be to 2 decimal places
