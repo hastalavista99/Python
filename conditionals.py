2805a811fff0e8706396d1f81cb2b19f1c855101
hastalavista <githumuj@gmail.com>
2023-07-12 12:09:57 +0300

    added conditionals

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+# > - greater than
+# >= - greater than or equal to
+# < - less than
+# >= - less than or equal to
+# = - assignment operator
+# == - equality operator
+# != - not equal
+
---
+++ function.py
@@ -0,0 +1,20 @@
+# Introduction to functions in python
+# the function argument can be given a default value
+def hello(to="world"):
+    print("hello,", to)
+
+
+hello()
+name = input("What is your name: ")
+hello(name)
+
+# Return Keyword
+
+def main():
+    a = int(input("Enter a number: "))
+    print("a squared is", square(a))
+
+def square(n):
+    return n*n # or pow(n, 2) also does the trick
+
+main()
