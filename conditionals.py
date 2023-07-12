95f09cef8e733180c666e5c8caeacd23a4e080e3
hastalavista <githumuj@gmail.com>
2023-07-12 13:19:08 +0300

    added if statement

--- conditionals.py
+++ conditionals.py
@@ -1,8 +1,22 @@
-# > - greater than
-# >= - greater than or equal to
-# < - less than
-# >= - less than or equal to
-# = - assignment operator
-# == - equality operator
-# != - not equal
+"""
+    Conditional Operators;
+    > - greater than
+    >= - greater than or equal to
+    < - less than
+    >= - less than or equal to
+    = - assignment operator
+    == - equality operator
+    != - not equal
+"""
 
+# IF statement
+
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a is equal to b")
