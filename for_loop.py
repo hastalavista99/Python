b09eb6932d0a388ad22b49fa220146306f9b5cc5
hastalavista <githumuj@gmail.com>
2023-07-17 16:13:55 +0300

    added for loop

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+# for loops are very effective in iterating through items in a list
+# the range function gives a list upto but not including the number between the parantheses
+# its better to use an underscore in the for loop if the variable wont be used again as below
+
+for _ in [0, 1, 2]:
+    print("meow")
--- while_loop.py
+++ while_loop.py
@@ -1,7 +1,15 @@
 
 # while loop executes the statement provided the condition is true
-i = 5
+# i = 5
 
-while i !=0:
+# while i !=0:
+#     print("meow")
+#     i -= 1
+
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
     print("meow")
-    i -= 1
