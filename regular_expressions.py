6cb9c866eedf86fd860f6cc9ced185f47d7b05af
hastalavista <githumuj@gmail.com>
2023-07-25 17:55:37 +0300

    added more regular expressions

--- regular_expressions.py
+++ regular_expressions.py
@@ -8,5 +8,18 @@
     ?   = 0 or 1 repetition
     {m} = m repetitions
     {m,n}= m-n repetitions(range)
+    ^   = matches the start of a string
+    $   = matches the end of string
+    []  = set of characters
+    [^] = complementing the set(all characters except)
+    \w  = any word character
+    \W  = now a word character, as well as numbers and the underscore
+    \s  = whitespace characters
+    \S  = not a whitespace character
+    \d  = decimal digit
+    \D  = not a decimal digit
+    A|B = either A or B
+    (...)= a group
+    (?:...) = non-capturing version
 """
 
--- validate.py
+++ validate.py
@@ -1,8 +1,8 @@
 import re
 
 email = input("What's your email? ")
-
-if re.search(r".+@.+\.com", email):
+# the 'r' tells python to treat the string as a raw string, ignoring escape characters
+if re.search(r"^\w+@\w+\.(edu|gov|com)$", email):
     print("Valid")
 else:
     print("Invalid")
